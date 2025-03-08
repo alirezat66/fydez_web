6c094643996bbbb85cb687db238f11b0a836b48c
Reza Taghizadeh <alirezataghizadeh@gmail.com>
2025-03-08 18:14:34 +0100

    version 2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,8 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1853652197"
-
+    serviceWorkerVersion: "1706646401"
   }
-  
 });
